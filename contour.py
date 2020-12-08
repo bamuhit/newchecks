eef45d49bb03d9989563f7f62c4e7beab78ff7c9
baig <bamuhit@yahoo.com>
2020-12-08 01:00:16 -0600

    Made the contour plot better

--- contour.py
+++ contour.py
@@ -5,18 +5,33 @@ Created on Tue Dec  8 00:05:14 2020
 @author: baig
 """
 
+###################################
+##### Plotting Contour Plot #######
+###################################
+
+''' Importing modules '''
 import numpy as np
 import matplotlib.pyplot as plt
 
 ''' Define a function for 2D '''
-
 def f(x, y):
     return np.cos(x) + np.sin(y)
+
 
 x = np.linspace(0, 5, 200)
 y = np.linspace(0, 5, 200)
 X, Y = np.meshgrid(x, y)
 Z = f(X,Y)
 
-fig = plt.figure(figsize = (6, 4))
+''' Plotting contour '''
+figW = 3.5
+GR = 1.609
+figH = 3.5/GR
+fig = plt.figure(figsize = (figW, figH))
 plt.contour(X, Y, Z, 200, cmap = 'RdBu')
+plt.colorbar(extend = 'both', ticks = np.linspace(np.min(Z), np.max(Z), 5))
+plt.xlabel('x (unit)', fontsize = 14)
+plt.ylabel('y (unit)', fontsize = 14)
+plt.xticks(np.linspace(0, 5, 6), fontsize = 12)
+plt.yticks(np.linspace(0, 5, 6), fontsize = 12)
+fig.tight_layout()
